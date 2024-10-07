2feaac7658ea22d0d1429294f2fe04d5ae74fc37
jwisniewski <Janusz.Wisniewski@port.lukasiewicz.gov.pl>
2024-10-07 12:03:41 +0200

    added PORTEKrun wrapper and new project creation

---
+++ PORTEKrun.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+import shutil
+
+parser = argparse.ArgumentParser(
+    description="Main PORT-EK v2 program. Use it to run all PORT-EK functions with the appropriate 'tool' argument."
+)
+parser.add_argument("tool", help="Name of the PORT-EK function you want to execute. Choose one of: new, find_k, enriched, map, classify")
+
+parser.add_argument(
+    "--project_dir",
+    help="path to the desired new project directory for use with PORTEKrun.py new",
+    type=str,
+)
+
+
+def _new_project(project_dir: str) -> None:
+    if os.path.isdir(project_dir) == True:
+        raise FileExistsError("This project directory already exists! PORT-EK does not allow overwriting projects. If you REALLY want to overwrite, remove the existing directory manually!")
+    os.mkdir(project_dir)
+    os.mkdir(f"{project_dir}/input")
+    os.mkdir(f"{project_dir}/output")
+    os.mkdir(f"{project_dir}/temp")
+    shutil.copy2("./templates/config.yaml", project_dir)
+    shutil.copy2("./templates/generate_kmers.sh", project_dir)
+    print(f"New empty PORT-EK project created in {project_dir}. Please edit config.yaml and generte_kmers.sh files as required and copy input fasta files into {project_dir}/input.")
+
+
+def main():
+    args = parser.parse_args()
+    if args.tool == "new":
+        if type(args.project_dir) != str:
+            raise ValueError("Please provide a valid project directory name.")
+        _new_project(args.project_dir)
+
+
+if __name__ == "__main__":
+    main()
+
