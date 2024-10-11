469ee209947b2ac860869814553922fb89ad0669
jwisniewski <Janusz.Wisniewski@port.lukasiewicz.gov.pl>
2024-10-11 13:58:59 +0200

    renamed PORTEKfind.py and moved to portek directory

--- templates/generate_kmers.sh
+++ templates/generate_kmers.sh
@@ -5,10 +5,10 @@ do
     mkdir -p "$project_dir"/input/"$k"mer_indices
     #Uncomment the following lines and change input file and group names as desired.
     #If you want more that 2 groups just copy and paste more lines below.
-    #Type 'python PORTEKfind.py -h' for more info about PORTEKfind options
+    #Type 'python ./portek/portek_get_kmers.py -h' for more info about k-mer finding options
 
-    # python PORTEKfind.py "$project_dir"/input/GROUP1.fasta "$project_dir"/input --k "$k" --group GROUP1 &
-    # python PORTEKfind.py "$project_dir"/input/GROUP2.fasta "$project_dir"/input --k "$k" --group GROUP2 &
+    # python ./portek/portek_get_kmers.py "$project_dir"/input/GROUP1.fasta "$project_dir"/input --k "$k" --group GROUP1 &
+    # python ./portek/portek_get_kmers.py "$project_dir"/input/GROUP2.fasta "$project_dir"/input --k "$k" --group GROUP2 &
 
 
 done
