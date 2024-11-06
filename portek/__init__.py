841c211ae3bf2e0e2d1f9c8581757e185a41f7ae
jwisniewski <Janusz.Wisniewski@port.lukasiewicz.gov.pl>
2024-11-06 14:34:55 +0100

    renamed portek.py to portek_utils.py

--- portek/__init__.py
+++ portek/__init__.py
@@ -1,17 +1,17 @@
-from .portek import encode_kmer
-from .portek import decode_kmer
-from .portek import filter_kmers
-from .portek import calc_kmer_pvalue
-from .portek import assign_kmer_group_ava, assign_kmer_group_ovr
-from .portek import check_exclusivity
-from .portek import build_similarity_graph_two_list
-from .portek import calc_agg_freq
-from .portek import map_kmers_find_mutations
-from .portek import assemble_kmers
-from .portek import plot_segments
-from .portek import plot_kmers_by_genome
-from .portek import assign_gene_from_interval, assign_gene_from_position
-from .portek import make_ordinal
+from .portek_utils import encode_kmer
+from .portek_utils import decode_kmer
+from .portek_utils import filter_kmers
+from .portek_utils import calc_kmer_pvalue
+from .portek_utils import assign_kmer_group_ava, assign_kmer_group_ovr
+from .portek_utils import check_exclusivity
+from .portek_utils import build_similarity_graph_two_list
+from .portek_utils import calc_agg_freq
+from .portek_utils import map_kmers_find_mutations
+from .portek_utils import assemble_kmers
+from .portek_utils import plot_segments
+from .portek_utils import plot_kmers_by_genome
+from .portek_utils import assign_gene_from_interval, assign_gene_from_position
+from .portek_utils import make_ordinal
 
 from .portek_findk import KmerFinder
 from .portek_findk import FindOptimalKPipeline
